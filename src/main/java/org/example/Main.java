707c71a860ce3b7ba19558587e62fed5c29014d4
amirxanrt <amirxanrt@hotmail.com>
2022-06-04 09:24:29 +0300

    deleted faker permanently

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,13 +1,8 @@
 package org.example;
 
-import com.github.javafaker.Faker;
 
 public class Main {
     public static void main(String[] args) {
-        Faker faker = new Faker();
-        String card = faker.finance().creditCard();
-        System.out.println("card = " + card);
-
-
+        System.out.println("hello world");
     }
 }
